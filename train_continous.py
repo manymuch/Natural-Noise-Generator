111361384570583d3f5358e31ef2f2459e527abd
manymuch <zjx219@gmail.com>
2019-03-21 09:49:11 -0400

    yG_z

--- train_continous.py
+++ train_continous.py
@@ -52,7 +52,7 @@ def train(fps, args):
         print(y.get_shape())
         G_z = tf.concat([y,G_z],1)
         G_z = tf.layers.conv1d(G_z, 1, args.wavegan_genr_pp_len, use_bias=False, padding='same')
-        G_z = tf.slice(G_z,[0,0,0],[-1,args.data_slice_len,-1])
+        G_z = tf.slice(G_z,[0,0,0],[-1,args.data_slice_len-args.wavegan_genr_pp_len,-1])
         print(G_z.get_shape())
   G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
 
@@ -96,7 +96,8 @@ def train(fps, args):
 
   # Make fake discriminator
   with tf.name_scope('D_G_z'), tf.variable_scope('D', reuse=True):
-    D_G_z = WaveGANDiscriminator(G_z, **args.wavegan_d_kwargs)
+    yG_z = tf.concat(y,G_z,1)
+    D_G_z = WaveGANDiscriminator(yG_z, **args.wavegan_d_kwargs)
 
   # Create loss
   D_clip_weights = None
