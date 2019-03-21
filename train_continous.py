7b1295cf060694e9f9fa41686b6dacd5934563b9
manymuch <zjx219@gmail.com>
2019-03-21 09:43:11 -0400

    shape

--- train_continous.py
+++ train_continous.py
@@ -48,8 +48,9 @@ def train(fps, args):
     if args.wavegan_genr_pp:
       with tf.variable_scope('pp_filt'):
         print(x.get_shape())
-        y = tf.slice(x,[0,0],[args.wavegan_genr_pp_len,0])
-        G_z = tf.concat([y,G_z],0)
+        y = tf.slice(x,[0,0,0],[-1,args.wavegan_genr_pp_len,-1])
+        print(y.get_shape())
+        G_z = tf.concat([y,G_z],1)
         G_z = tf.layers.conv1d(G_z, 1, args.wavegan_genr_pp_len, use_bias=False, padding='same')
         print(G_z.get_shape())
   G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
