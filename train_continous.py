9bcd96b1e255c3e9ffde3be5dcae42b85d60dada
manymuch <zjx219@gmail.com>
2019-03-21 09:51:06 -0400

    y scope name

--- train_continous.py
+++ train_continous.py
@@ -96,6 +96,7 @@ def train(fps, args):
 
   # Make fake discriminator
   with tf.name_scope('D_G_z'), tf.variable_scope('D', reuse=True):
+    y = tf.slice(x,[0,0,0],[-1,args.wavegan_genr_pp_len,-1])
     yG_z = tf.concat(y,G_z,1)
     D_G_z = WaveGANDiscriminator(yG_z, **args.wavegan_d_kwargs)
 
