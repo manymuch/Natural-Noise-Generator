b665b1d659648aa3f5993f50dd83acbe2a1eb299
manymuch <zjx219@gmail.com>
2019-04-22 13:19:21 -0400

    test write file

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from librosa.output import write_wav as wav_w
 from librosa.core import load as wav_r
@@ -5,8 +6,17 @@ from librosa.core import load as wav_r
 
 def main():
     file_path = "beach/beach.wav"
-    demo_wave = wav_r(file_path,sr=11025,duration=32768/11025)[0]
+    output_dir = "beach_sliced"
+    sample_rate = 11025
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+        
+    demo_wave = wav_r(file_path,sr=sample_rate,duration=32768/sample_rate)[0]
     print(demo_wave.shape)
+    wav_w(os.path.join(output_dir,str(3)+'.wav'), demo_wave, sample_rate)
+
+
+
 
 
 if __name__ == '__main__':
