3df7034cb5927ee85bd1e7ee90cf27bcb8a5bb17
manymuch <zjx219@gmail.com>
2019-04-26 09:52:49 -0400

    get demo

--- preprocess.py
+++ preprocess.py
@@ -9,15 +9,16 @@ def main():
 
     output_dir = "beach_sliced"
     sample_rate = 11025
+    output_lenght = 32768
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
     whole_audio = wav_r(whole_audio_path)[1]
-    birds = wav_r("birds/birds11025.wav")[1]
     print(whole_audio.shape)
     print(whole_audio.dtype)
-    print(birds.shape)
-    print(birds.dtype)
-    one_wave = whole_audio[sample_rate*1,sample_rate*2]
+
+    one_wave = whole_audio[output_lenght*1:output_lenght*2]
+    print(one_wave)
+    wav_w("demo.wav",sample_rate,one_wave)
 
 
 
