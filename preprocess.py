72c79a54da498fd0055cdbb54c80e9bf545753b0
manymuch <zjx219@gmail.com>
2019-04-26 10:23:30 -0400

    find max

--- preprocess.py
+++ preprocess.py
@@ -24,6 +24,8 @@ def main():
     print(convolve_result.shape)
     print(convolve_result.max(axis=0))
     print(convolve_result.mean())
+    picked_index = np.where(convolve_result>32768/2)
+    print(picked_index.shape)
 
 
 
